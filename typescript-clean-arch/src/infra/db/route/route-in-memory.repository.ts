fe436407ad073c26f2d31c1869f62bb072ffadbe
André Pereira <andrepereira97@yahoo.com>
2022-06-27 08:15:32 -0300

    :sparkles: add public identifier

--- typescript-clean-arch/src/infra/db/route/route-in-memory.repository.ts
+++ typescript-clean-arch/src/infra/db/route/route-in-memory.repository.ts
@@ -3,11 +3,11 @@ import { Route, RouteRepositoryInterface } from '../../../domain';
 export class RouteInMemoryRepository implements RouteRepositoryInterface {
   public items: Route[] = [];
 
-  async store(route: Route): Promise<void> {
+  public async store(route: Route): Promise<void> {
     this.items.push(route);
   }
 
-  async findAll(): Promise<Route[]> {
+  public async findAll(): Promise<Route[]> {
     return this.items;
   }
 }
